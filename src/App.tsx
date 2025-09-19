623534ec9ce9d1240eea021adb64213b5fadb12e
Wittwizz <workxdipanshu@gmail.com>
2025-09-19 22:12:12 +0530

    Remove legacy header navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import AppRouter from './components/AppRouter';
 import SmoothBackground from './ui/SmoothBackground';
-import Navigation from './ui/Navigation';
 
 export default function App() {
   console.log('🚀 App component rendering - Smooth Background System');
@@ -9,7 +8,6 @@ export default function App() {
   return (
     <SmoothBackground className="min-h-screen relative overflow-hidden">
       {/* Smooth Background System */}
-      <Navigation />
       <main className="pt-20">
         <AppRouter />
       </main>
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -9,8 +9,6 @@ export { default as FullPageScroll } from './FullPageScroll';
 
 // Page Transition Components
 export { default as PageTransition } from './PageTransition';
-export { default as Navigation } from './Navigation';
-
 // Note: Other components moved to src/ui/unused/ for future use
 
 
